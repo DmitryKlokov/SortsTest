d5d6261686a26ff16faff387d37c4ca3182f5b7b
Дмитрий <dmitryklokov95@gmail.com>
2015-10-15 14:10:33 +0300

    added an abstract class, which is a copy of any container elements into an array, sort the resulting array and copy the elements of array in the original container

---
+++ SortsTest/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortsTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<int> li = new List<int>();
+            Random r = new Random();
+            for (int i = 0; i < 23; i++)
+            {
+                li.Add(r.Next(0, 344));
+            }
+            foreach(int i in li)
+            {
+                richTextBox1.Text+=i.ToString()+" ";
+            }
+            richTextBox1.Text+="\n";
+            GenericSortBubble<int> gsb = new GenericSortBubble<int>();
+            li = gsb.Sort(li);
+            foreach(int i in li)
+            {
+                richTextBox1.Text+=i.ToString()+" ";
+            }
+            richTextBox1.Text+="\n";
+        }
+    }
+}
---
+++ SortsTest/GenericSortBubble.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortsTest
+{
+    class GenericSortBubble<T> : AbstractClassForSorts<T>
+    {
+        protected override void algorithm()
+        {
+            for (int i = 0; i < collection.Count() - 1; i++)
+            {
+                for (int j = i + 1; j < collection.Count(); j++)
+                {
+                    //if (collection[i].CompareTo(collection[j]) == 0)
+                    //{
+                    //    T tmp = collection[i];
+                    //    collection[i] = collection[j];
+                    //    collection[j] = tmp;
+                    //}
+                }
+            }
+        }
+    }
+}
