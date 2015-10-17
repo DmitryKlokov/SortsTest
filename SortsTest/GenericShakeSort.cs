7cbdb0dffa0e19111d70c38920f5a3150fa5916f
Дмитрий <dmitryklokov95@gmail.com>
2015-10-17 16:28:01 +0300

    add sorts

---
+++ SortsTest/GenericShakeSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortsTest
+{
+    class GenericShakeSort<T> : AbstractClassForSorts<T> where T : IComparable
+    {
+        protected override void algorithm()
+        {
+            for (int i = 0; i < collection.Count() / 2; i++)
+            {
+                int beg = 0;
+                int end = collection.Count() - 1;
+
+                do
+                {
+                    if (collection[beg].CompareTo(collection[beg + 1]) == 1)
+                    {
+                        Swap(collection, beg, beg + 1);
+                    }
+                    beg++;
+
+                    if (collection[end - 1].CompareTo(collection[end]) == 1)
+                    {
+                        Swap(collection, end - 1, end);
+                    }
+                    end--;
+
+                } while (beg <= end);
+            }
+        }
+        private void Swap(T[] collection, int i, int j)
+        {
+            T item = collection[i];
+            collection[i] = collection[j];
+            collection[j] = item;
+        }
+    }
+}
--- SortsTest/Program.cs
+++ SortsTest/Program.cs
@@ -10,50 +10,42 @@ namespace SortsTest
     {
         static void Main(string[] args)
         {
-            testListInt();
+            Test();
         }
 
-        public static void testListInt()
-        {
-            List<int> li1 = new List<int>();
-            List<int> li2 = new List<int>();
-            List<int> li3 = new List<int>();
-
-            Random r = new Random();
-            for (int i = 0; i < 34; i++)
-            {
-                int k = r.Next(0, 50);
-                int k1 = r.Next(0, 50);
-                int k2 = r.Next(0, 50);
-                li1.Add(k);
-                li2.Add(k1);
-                li3.Add(k2);
-            }
-            Console.Write("Before\n");
-            foreach (int i in li1)
+        public static void ShowList<T>(List<T> list)
+        {            
+            foreach (T i in list)
             {
                 Console.Write(i.ToString() + " ");
             }
             Console.Write("\n\n\n");
+        }
+
+
+
+        public static void Test()
+        {
+            List<int> li1 = new List<int>() { 1,43,54,4,67,888,3,2,4,6,65,34,23,36,23,52};
+            List<int> li2 = new List<int>() { 1, 43, 54, 4, 67, 888, 3, 2, 4, 6, 65, 34, 23, 36, 23, 52 };
+            List<int> li3 = new List<int>() { 1, 43, 54, 4, 67, 888, 3, 2, 4, 6, 65, 34, 23, 36, 23, 52 };
+
+            Console.Write("Before\n");
+            ShowList<int>(li1);
 
             //bubble
             GenericSortBubble<int> gsb = new GenericSortBubble<int>();
             li1 = gsb.Sort(li1);
-            foreach (int i in li1)
-            {
-                Console.Write(i.ToString() + " ");
-            }
-            Console.Write("\n\n\n");
+            ShowList<int>(li1);
 
-            //QuickSort
-            GenericQuickSort<int> gqs = new GenericQuickSort<int>();
-            li2 = gqs.Sort(li2);
+            //ShakeSort
+            GenericShakeSort<int> gss = new GenericShakeSort<int>();
+            li2 = gss.Sort(li2);
+            ShowList<int>(li2);
 
-            foreach (int i in li2)
-            {
-                Console.Write(i.ToString() + " ");
-            }
-            Console.Write("\n\n");
+            GenericHeapSort<int> ghs = new GenericHeapSort<int>();
+            li3 = ghs.Sort(li3);
+            ShowList<int>(li3);
         }
     }
 }
